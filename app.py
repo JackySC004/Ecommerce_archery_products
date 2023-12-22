d4cd0079f0a9a98367cf2f4995de4ba0a6f087d3
JackySC004 <luanasosa004@gmail.com>
2023-12-23 00:48:49 +0700

    create table user and database service

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 from flask import Flask, request, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 
 app=Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///database.db'
+app.config['SECRET_KEY']='stopuncle'
+db=SQLAlchemy(app)
+
+class User(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String(60))
+    email=db.Column(db.String(100))
+    password=db.Column(db.String(100))
+
+with app.app_context():
+    db.create_all()
 
 
 @app.route('/register', methods=['POST', 'GET'])
@@ -9,5 +23,9 @@ def register():
         name= request.form['name']
         email=request.form['email']
         password=request.form['password']
+        new_user=User(name=name, email=email,password=password)
+        db.session.add(new_user)
+        db.session.commit()
+
 
     return render_template('register.html')
